d73714bf8e3ee06affb47618d2a7a03803fb7d1a
keerthana2624 <keerthanamummy2002@gmail.com>
2024-07-09 18:29:00 +0530

    `if-else` statement to check if a number is positive or negative.

---
+++ PositiveNegativeCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PositiveNegativeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+        if (number > 0) {
+            System.out.println("The number " + number + " is positive.");
+        } else if (number < 0) {
+            System.out.println("The number " + number + " is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
