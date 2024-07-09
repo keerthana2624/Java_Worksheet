34074fc3c24b6e1895aa567d319501fbed76ca31
keerthana2624 <keerthanamummy2002@gmail.com>
2024-07-09 18:16:18 +0530

    adding a setter method to the class and demonstrate changing the field's value.

--- Main.java
+++ Main.java
@@ -1,16 +1,43 @@
+// class MyClass {
+//     private int privateField;
+//     public MyClass(int value) {
+//         this.privateField = value;
+//     }
+//     public int getPrivateField() {
+//         return privateField;
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         MyClass obj = new MyClass(42);
+//         System.out.println("Value of privateField: " + obj.getPrivateField());
+//     }
+// }
+
+
 class MyClass {
     private int privateField;
+
     public MyClass(int value) {
         this.privateField = value;
     }
+
     public int getPrivateField() {
         return privateField;
+    }
+
+    public void setPrivateField(int value) {
+        this.privateField = value;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         MyClass obj = new MyClass(42);
-        System.out.println("Value of privateField: " + obj.getPrivateField());
+        System.out.println("Initial value of privateField: " + obj.getPrivateField());
+
+        obj.setPrivateField(100);
+        System.out.println("Updated value of privateField: " + obj.getPrivateField());
     }
 }
