63b8f4edf11cce485454ad2357816b40ce5a2d5d
keerthana2624 <keerthanamummy2002@gmail.com>
2024-07-09 21:29:00 +0530

    `for` loop to print the first 10 multiples of a given number.

---
+++ FirstTenMultiples.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FirstTenMultiples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println("First 10 multiples of " + number + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
